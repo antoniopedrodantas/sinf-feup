389ea154c18029c93833a69656fc6f695d41df32
vitorhugo13 <vhlg99@gmail.com>
2020-11-22 18:48:26 +0000

    Finished login scene; fixed chrome bugs

--- tempura/client/src/scenes/Login/Login.tsx
+++ tempura/client/src/scenes/Login/Login.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './styles/Login.css';
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faLock} from '@fortawesome/free-solid-svg-icons'
+const logo =  require("../../assets/logo.svg")
 
 const Login: React.FC = () => {
   return (
     <>
       <div className="login">
         <div className="loginContainer">
-
-          {/*TODO: place logo here (não sei porque mas dá-me erro ao introduzir o svg*/}
-
+          <img id="logo" src={logo} alt="logo" />
           <hr id="loginHR" />
           <div id="loginText"> Login into your account</div>
 
@@ -18,12 +17,12 @@ const Login: React.FC = () => {
             
             <div className="credentials">
               <div className ="username">
-                <span className="icon">i</span>
-                <input />
+                <span className="icon1"><FontAwesomeIcon icon={faUser} /></span>
+                <input type="text" id="username" placeholder="username" />
               </div>
               <div className="password">
-                <span className="icon">i</span>
-                <input />
+                <span className="icon2"><FontAwesomeIcon icon={faLock} /> </span>
+                <input type="password" id="password" placeholder="password" />
               </div>
             </div>
 
