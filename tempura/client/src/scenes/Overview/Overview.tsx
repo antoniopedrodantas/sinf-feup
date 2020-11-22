e722e20180ec3ba04f67f4ba15964dbd19dda77a
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2020-11-22 17:30:37 +0000

    Created single value card react component and some css

--- tempura/client/src/scenes/Overview/Overview.tsx
+++ tempura/client/src/scenes/Overview/Overview.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SingleValueCard from 'src/components/SingleValueCard/SingleValueCard';
 import './styles/Overview.css';
 
 const Overview: React.FC = () => {
   return (
     <>
-      <p className="overview"> This is the Overview page :o </p>
+      <SingleValueCard type="text" title="Titulo" value="3"/>
+      <SingleValueCard type="text" title="Titulo" value="2"/>
     </>
   );
 };
