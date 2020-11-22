a8e7049a0714c212da8cedff2ffb7fdbb058610a
vitorhugo13 <vhlg99@gmail.com>
2020-11-22 17:19:23 +0000

    Working on login; some bugs only in chrome

--- tempura/client/src/scenes/Login/Login.tsx
+++ tempura/client/src/scenes/Login/Login.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import './styles/Login.css';
 
+
+
 const Login: React.FC = () => {
   return (
     <>
       <div className="login">
-        <div className="loginForm">
-          login form here
+        <div className="loginContainer">
+
+          {/*TODO: place logo here (não sei porque mas dá-me erro ao introduzir o svg*/}
+
+          <hr id="loginHR" />
+          <div id="loginText"> Login into your account</div>
+
+          <form className="loginForm">
+            
+            <div className="credentials">
+              <div className ="username">
+                <span className="icon">i</span>
+                <input />
+              </div>
+              <div className="password">
+                <span className="icon">i</span>
+                <input />
+              </div>
+            </div>
+
+            {/*TODO: add onclick event later */}
+            <button className="loginButton" type="submit"> 
+              <span> Log in</span>
+            </button>
+          </form>
+
         </div>
       </div>
     </>
