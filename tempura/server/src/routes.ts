1be2b7085e09e0debdc861e395a0de982b199c86
Bernardo Santos <bernardosantos670@gmail.com>
2020-11-25 17:19:32 +0000

    Added product endpoints

--- tempura/server/src/routes.ts
+++ tempura/server/src/routes.ts
@@ -1,13 +1,15 @@
 import express from "express";
 
 import client from "./routes/client";
-import supplier from "./routes/supplier"
+import product from "./routes/product";
+import supplier from "./routes/supplier";
 
 
 const router = express.Router();
 
 
 router.use('/client', client);
+router.use('/product', product);
 router.use('/supplier', supplier);
 
 
