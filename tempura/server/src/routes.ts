1807d878627b211add90f82fa9af0ffe521bdc0d
Bernardo Santos <bernardosantos670@gmail.com>
2020-11-25 17:45:11 +0000

    Added stock endpoints

--- tempura/server/src/routes.ts
+++ tempura/server/src/routes.ts
@@ -3,6 +3,9 @@ import express from "express";
 import client from "./routes/client";
 import product from "./routes/product";
 import supplier from "./routes/supplier";
+
+import stock from "./routes/stock";
+import financial from "./routes/financial";
 import miscellaneous from "./routes/misc";
 
 
@@ -12,6 +15,9 @@ const router = express.Router();
 router.use('/client', client);
 router.use('/product', product);
 router.use('/supplier', supplier);
+
+router.use('/', stock);
+router.use('/', financial);
 router.use('/', miscellaneous);
 
 
