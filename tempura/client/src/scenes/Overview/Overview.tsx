3fbf417067f8ca2f3b300796300b27af4d1c7186
vitorhugo13 <vhlg99@gmail.com>
2020-11-30 17:26:22 +0000

    playing with tables

--- tempura/client/src/scenes/Overview/Overview.tsx
+++ tempura/client/src/scenes/Overview/Overview.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './styles/Overview.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Overview: React.FC = () => {
   return (
     <>
-      <p className="overview"> This is the Overview page :o </p>
+
+      <table className="table table-dark table-borderless table-sm">
+      <thead>
+        <tr className="table-header">
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
     </>
   );
 };
