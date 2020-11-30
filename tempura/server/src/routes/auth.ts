aa023769a54bd367de024db59fb6584ea03e9857
Bernardo Santos <bernardosantos670@gmail.com>
2020-11-30 13:13:57 +0000

    Docs route up and running

--- tempura/server/src/routes/auth.ts
+++ tempura/server/src/routes/auth.ts
@@ -3,56 +3,6 @@ import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
 
 
-/**
- * @swagger
- * /login:
- *   post:
- *     tags:
- *     - "auth"
- *     summary: "Log in to the service"
- *     description: ""
- *     operationId: "Login"
- *     requestBody:
- *       description: "User to log in"
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - username
- *               - password
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *                 format: password
- *           example:
- *             username: bernas
- *             password: pass123
- *     responses:
- *       200:
- *         description: Successfully logged in
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               required:
- *                 - token
- *               properties:
- *                 token:
- *                   type: string
- *             example:
- *               token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2Mzh9.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI"
- *       default:
- *         description: unexpected error
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Error'
- *     security: []
- */
 router.post('/login', login);
 router.post('/logout', logout);
 
--- tempura/server/src/routes/docs.ts
+++ tempura/server/src/routes/docs.ts
@@ -1,12 +1,31 @@
 import express from "express";
 import swaggerUI from "swagger-ui-express";
-import swaggerSpec from "../swagger";
+import YAML from "yamljs";
+import { root } from "../path";
+
+
+const PORT = process.env.PORT || 8000
+
+const swaggerDoc = YAML.load(`${root}/docs/api.yml`);
+const swaggerDef = {
+    servers: [
+        {
+            url: `http://localhost:${PORT}`,
+            description: "Development Server"
+        }
+    ]
+}
+const options = {
+    swaggerDef,
+    customCss: '.swagger-ui .topbar { display: none }'
+    // explorer: true
+};
 
 
 const router = express.Router();
 
-
-router.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+router.use('/docs', swaggerUI.serve);
+router.get('/docs', swaggerUI.setup(swaggerDoc, options));
 
 
 export default router;
