9919777fa3e80819bc2660d5c87842c4647f50a4
Bernardo Santos <bernardosantos670@gmail.com>
2020-12-03 20:23:11 +0000

    Separated parser related code to a different file

---
+++ tempura/server/src/lib/parser.ts
@@ -0,0 +1,58 @@
+import { JsonObject } from "swagger-ui-express";
+
+
+
+function parseJSON(jsonObj: JsonObject) {
+    const header = jsonObj["Header"];
+
+    delete jsonObj["Header"];
+
+    // convert GeneralLedgerAccounts
+    let accounts = parseGeneralLedgerAccounts(jsonObj["MasterFiles"]["GeneralLedgerAccounts"]);
+    jsonObj["MasterFiles"]["GeneralLedgerAccounts"] = accounts;
+
+
+    if (header["TaxAccountingBasis"] === 'C') {
+        // accountability
+
+        // only need the accounts ?
+    } else if (header["TaxAccountingBasis"] === 'F') {
+        // billing
+
+    }
+}
+
+
+function parseGeneralLedgerAccounts(old: JsonObject) {
+    let taxonomyCodes: JsonObject = {};
+    let accounts: JsonObject = {};
+
+    let oldAccounts: Array<JsonObject> = old["Account"];
+    oldAccounts.forEach(element => {
+        let id = element["AccountID"];
+
+        delete element["AccountID"]
+        let info = element;
+
+        if (info.hasOwnProperty('TaxonomyCode')) {
+            let code = info["TaxonomyCode"];
+            delete info["TaxonomyCode"];
+
+            if (taxonomyCodes.hasOwnProperty(code)) {
+                taxonomyCodes[code].push(id);
+            } else {
+                taxonomyCodes[code] = [id];
+            }
+        }
+
+        accounts[id] = info;
+    });
+
+    return {
+        "TaxonomyReference": `${old["TaxonomyReference"]}`,
+        "TaxonomyCodes": taxonomyCodes,
+        "Accounts": accounts
+    };
+}
+
+export default parseJSON;
