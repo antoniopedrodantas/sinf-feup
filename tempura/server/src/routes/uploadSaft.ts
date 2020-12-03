61a1e70145f59b57c9b4cf569bc6e72f9a7cf74d
Bernardo Santos <bernardosantos670@gmail.com>
2020-12-03 20:25:21 +0000

    Refactored upload logic

--- tempura/server/src/routes/uploadSaft.ts
+++ tempura/server/src/routes/uploadSaft.ts
@@ -4,11 +4,17 @@ import { root } from "../path";
 // import libxml from "libxmljs";
 import fs from "fs";
 import parser from "xml2json";
-import { JsonObject } from "swagger-ui-express";
 
+import { formatDate, formatDateString } from "../lib/date";
 
 import { getRepository } from "typeorm";
 import { Saft } from "../entity/Saft";
+
+import parseJSON from "../lib/parser";
+import HttpException from "../exceptions/HttpException";
+
+import asyncMiddleware from "../middlewares/asyncMiddleware";
+import { JsonObject } from "swagger-ui-express";
 
 
 const router = express.Router();
@@ -16,183 +22,121 @@ const router = express.Router();
 const xsdPath = `${root}/safts/saftpt1.04_01.xsd`;
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, `${root}/safts/tmp/`);
-  },
-  filename: (req, file, cb) => {
-    // TODO add name scheme or name generator
-    cb(null, file.originalname);
-  }
-})
-
-
-const filefilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-  if (file.mimetype != 'application/xml') {
-    cb(new Error('Wrong file type.'))
-  } else {
-    // TODO: add validacao xsd aqui 
-    // commented code is here for when we get back to this
-
-    // console.log(file.buffer);
-    // const xmlDoc = libxml.parseXml((file.buffer).toString());
-    // const xsdDoc = libxml.parseXml(fs.readFileSync(xsdPath).toString());
-
-    // if (xmlDoc.validate(xsdDoc)) {
-    //   console.log('valid xml');
-    //   cb(null, true);
-    // } else {
-    //   console.log('not a valid xml');
-    //   cb(new Error('Not a valid SAFT-PT file.'));
-    // }
-
-    cb(null, true)
-  }
-}
+	destination: (request, file, next) => {
+		next(null, `${root}/safts/tmp/`);
+	},
+	filename: (request, file, next) => {
+		// TODO: add name scheme or name generator
+		// TODO: use user id or smth
+		next(null, `${Math.floor(Date.now() / 1000)}`);
+	}
+});
 
 const upload = multer({
-  storage: storage,
-  fileFilter: filefilter,
+	storage: storage,
+	fileFilter: filefilter,
 }).single('saft');
 
+
 // TODO: Auth middleware
-router.post('/upload', uploadfile);
+router.post('/upload', upload, asyncMiddleware(uploadfile));
 
 
+function filefilter(request: Request, file: Express.Multer.File, next: multer.FileFilterCallback) {
+	if (file.mimetype != 'application/xml') {
+		next(new HttpException(415, 'Wrong file extension, not an xml file.'))
+	}
+	// TODO: add validacao xsd aqui 
+	// commented code is here for when we get back to this
 
-function uploadfile(request: Request, response: Response, next: NextFunction) {
-  upload(request, response, (error: any) => {
-    if (error instanceof Error) {
-      response.status(400).send({
-        error: true,
-        message: error.message
-      });
-    } else if (error) {
-      response.status(500)
-    } else {
+	// console.log(file.buffer);
+	// const xmlDoc = libxml.parseXml((file.buffer).toString());
+	// const xsdDoc = libxml.parseXml(fs.readFileSync(xsdPath).toString());
 
-      // xml to json
-      console.log(request.file.filename);
-      const xmlData = fs.readFileSync(`${root}/safts/tmp/${request.file.filename}`);
-      let jsonObj = JSON.parse(parser.toJson(xmlData));
+	// if (xmlDoc.validate(xsdDoc)) {
+	//   console.log('valid xml');
+	//   cb(null, true);
+	// } else {
+	//   console.log('not a valid xml');
+	//   cb(new Error('Not a valid SAFT-PT file.'));
+	// }
 
-      // delete tmp file
-      fs.unlinkSync(`${root}/safts/tmp/${request.file.filename}`);
-
-      // remove header from json
-      jsonObj = jsonObj["AuditFile"];
-      let header = jsonObj["Header"];
-
-      // get header info
-      // TODO: come up with some sort of naming scheme
-      let fileName = "test.json"; // FIXME: this is temporary
-
-      // get header information
-      const saftHeaderInfo: SaftHeader = {
-        name: fileName,
-        creationDate: header.DateCreated,
-        startDate: header.StartDate,
-        endDate: header.EndDate,
-        fiscalYear: header.FiscalYear,
-        type: header.TaxAccountingBasis,
-        path: `${root}/safts/${fileName}`
-      }
-      console.log(saftHeaderInfo);
-
-      // TODO: check if file overlaps with any already in the db
-      // TODO: insert file entry into db with above information
-
-      // TODO: modify json schema, to make more accessible (may depends on saft type)
-
-      // remove header
-      delete jsonObj["Header"];
-
-      // convert GeneralLedgerAccounts
-      let accounts = convertGeneralLedgerAccounts(jsonObj["MasterFiles"]["GeneralLedgerAccounts"]);
-      jsonObj["MasterFiles"]["GeneralLedgerAccounts"] = accounts;
-
-
-      if (saftHeaderInfo.type === 'C') {
-        // accountability
-
-        // only need the accounts ?
-      } else if (saftHeaderInfo.type === 'F') {
-        // billing
-
-      } else {
-        // TODO: throw error SAFT type not supported
-
-      }
-
-
-
-
-      fs.writeFileSync(saftHeaderInfo.path, JSON.stringify(jsonObj));
-
-      response.status(200).send({
-        error: false,
-        message: 'Saft was uploaded successfuly!'
-      });
-    }
-  });
+	next(null, true)
 }
 
-function convertGeneralLedgerAccounts(old: JsonObject) {
-  let taxonomyCodes: JsonObject = {};
-  let accounts: JsonObject = {};
+async function uploadfile(request: Request, response: Response, next: NextFunction) {
 
-  let oldAccounts: Array<JsonObject> = old["Account"];
-  oldAccounts.forEach(element => {
-    let id = element["AccountID"];
+	// xml to json
+	console.log(request.file.filename);
+	const xmlData = fs.readFileSync(`${root}/safts/tmp/${request.file.filename}`);
+	let jsonObj = JSON.parse(parser.toJson(xmlData));
 
-    delete element["AccountID"]
-    let info = element;
+	// delete tmp file
+	fs.unlinkSync(`${root}/safts/tmp/${request.file.filename}`);
 
-    if (info.hasOwnProperty('TaxonomyCode')) {
-      let code = info["TaxonomyCode"];
-      delete info["TaxonomyCode"];
+	// remove header from json
+	jsonObj = jsonObj["AuditFile"];
+	const header = jsonObj["Header"];
 
-      if (taxonomyCodes.hasOwnProperty(code)) {
-        taxonomyCodes[code].push(id);
-      } else {
-        taxonomyCodes[code] = [id];
-      }
-    }
 
-    accounts[id] = info;
-  });
+	if (header["TaxAccountingBasis"] !== 'C' && header["TaxAccountingBasis"] !== 'F') {
+		throw next(new HttpException(400, 'SAFT type not supported'));
+	}
 
-  return {
-    "TaxonomyReference": `${old["TaxonomyReference"]}`,
-    "TaxonomyCodes": taxonomyCodes,
-    "Accounts": accounts
-  };
+	// get header info
+	// TODO: come up with some sort of naming scheme
+	let fileName = `${request.file.filename}.json`; // FIXME: this is temporary
+
+	const saftRepository = getRepository(Saft);
+
+	const safts = await saftRepository.find({
+		start_date: formatDateString(header.StartDate),
+		end_date: formatDateString(header.EndDate),
+		tax_accounting_basis: header.TaxAccountingBasis
+	});
+
+
+	if (safts.length === 0) {
+		await saftRepository.save(getSaftInfo(fileName, header));
+	} else if (safts.length === 1) {
+		if (formatDate(safts[0].created_at) >= formatDateString(header.DateCreated)) {
+			throw new HttpException(400, 'A more recent file has already been uploaded');
+		} else {
+			await saftRepository.delete({ id: safts[0].id })
+			await saftRepository.save(getSaftInfo(fileName, header));
+		}
+	} else {
+		throw next(new HttpException(500, 'Internal server error'))
+	}
+
+	// improve json schema
+	parseJSON(jsonObj);
+
+	fs.writeFileSync(`${root}/safts/${fileName}`, JSON.stringify(jsonObj));
+
+	response
+		.status(200)
+		.send({
+			error: false,
+			message: 'Saft was uploaded successfuly!'
+		});
+}
+
+function getSaftInfo(fileName: string, header: JsonObject) {
+	return {
+		name: fileName,
+		start_date: new Date(header.StartDate),
+		end_date: new Date(header.EndDate),
+		created_at: new Date(header.DateCreated),
+		path: `${root}/safts/${fileName}`,
+		fiscal_year: parseInt(header.FiscalYear, 10),
+		tax_accounting_basis: header.TaxAccountingBasis
+		// FIXME: add user information
+	}
 }
 
 
-interface SaftHeader {
-  name: string,
-  creationDate: string,
-  startDate: string,
-  endDate: string,
-  fiscalYear: string,
-  type: string,
-  path: string
-};
 
-// possible scenarios
-//  - overlaps, but the file is newer -> delete old file (disk and db) and insert new
-//  - overlaps, but the file is older -> keep old file and send error explaining the situation
-//  - overlaps, but the file is a different type -> insert new file
-//  - no overlap -> insert new file
-
-function checkOverlapping(info: SaftHeader) {
-  // how can a file completly overlap another?
-  //  - same start and end date 
-}
-
-function insertSaft(info: SaftHeader) {
-
-}
 
 
 export default router;
