494babf35b26fdb624477dc7f01683d9ed2f0eeb
vitorhugo13 <vhlg99@gmail.com>
2020-12-05 16:55:44 +0000

    Component finished, missing styling

--- tempura/client/src/components/DrillDownInfo/DrillDownInfo.tsx
+++ tempura/client/src/components/DrillDownInfo/DrillDownInfo.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import './styles/DrillDownInfo.css';
 
+interface InfoProps{
+    title: String;
+    fields: String[];
+    values: String[];
+}
 
-
-const DrillDownInfo: React.FC<> = () => {
+const DrillDownInfo: React.FC<InfoProps> = ({title,fields, values}) => {
   
-  return (
-    <>
-      
-    </>
+    return (
+        <>
+        <div className="info-drill">
+            <h3 className="table-title">{title}</h3>
+            <div className="info-values">
+                {fields.map((field, i) => {     
+                    return (<p className="info-p"><span className="info-field">{field}</span><span className="info-value">{values[i]}</span></p>) 
+                })}
+            </div>
+        </div>
+        </>
   );
 };
 
