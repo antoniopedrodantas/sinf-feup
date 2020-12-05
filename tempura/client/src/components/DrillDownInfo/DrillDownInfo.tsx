e73c62c8f1c50f0d196bc642d2dedf4621d0f280
vitorhugo13 <vhlg99@gmail.com>
2020-12-05 16:39:57 +0000

    Started client/supplier component

---
+++ tempura/client/src/components/DrillDownInfo/DrillDownInfo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles/DrillDownInfo.css';
+
+
+
+const DrillDownInfo: React.FC<> = () => {
+  
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default DrillDownInfo;
