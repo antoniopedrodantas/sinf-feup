7da70342df5412ca978c09d9d66cfa5078fdaf28
Pedro Dantas <pedro.dantascv@gmail.com>
2020-12-06 19:08:17 +0000

    Implemented CORS policy

--- tempura/server/src/main.ts
+++ tempura/server/src/main.ts
@@ -5,12 +5,22 @@ import * as bodyParser from "body-parser";
 import { User } from "./entity/User";
 import { root } from "./path";
 import router from "./routes";
+import cors from "cors";
+import morgan from "morgan";
 
 const options: ConnectionOptions = {
     type: "sqlite",
     database: `${root}/database/tempura.db`,
     entities: [ User ],
     logging: true
+}
+
+const corsOptions = {
+    "origin": "http://localhost:3000",
+    "methods": ["GET","PUT","POST","DELETE"],
+    "preflightContinue": false,
+    "credentials": true,
+    "optionsSuccessStatus": 200
 }
 
 createConnection(options)
@@ -22,28 +32,11 @@ createConnection(options)
 
         // setup express app here
         // ...
-        
-        app.use(function (req, res, next) {
-            
-            /*var err = new Error('Not Found');
-            err.status = 404;
-            next(err);*/
-            
-            // Website you wish to allow to connect
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            
-            // Request methods you wish to allow
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            
-            // Request headers you wish to allow
-            res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-            
-            //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            
-            // Pass to next layer of middleware
-            next();
-            
-        });
+
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(cors(corsOptions));
+        app.use(morgan('dev'));
 
         // ...
 
--- tempura/server/src/migration/1606344300862-seed.ts
+++ tempura/server/src/migration/1606344300862-seed.ts
@@ -2,12 +2,13 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class seed1606344300862 implements MigrationInterface {
 
+    // all passwords are 'pass123'
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`INSERT INTO user VALUES (1, 'bernas', 'pass123')`);
-        await queryRunner.query(`INSERT INTO user VALUES (2, 'vitinho', 'pass123')`);
-        await queryRunner.query(`INSERT INTO user VALUES (3, 'tone', 'pass123')`);
-        await queryRunner.query(`INSERT INTO user VALUES (4, 'filipe', 'pass123')`);
-        await queryRunner.query(`INSERT INTO user VALUES (5, 'leonor', 'pass123')`);
+        await queryRunner.query(`INSERT INTO user VALUES (1, 'bernas', '$2a$08$JA.JgQ4jSyv4rZn7FpZyfeJlStxLGD7b7aGEdz/6i8OxGyOIgYwTC')`);
+        await queryRunner.query(`INSERT INTO user VALUES (2, 'vitinho', '$2a$08$JA.JgQ4jSyv4rZn7FpZyfeJlStxLGD7b7aGEdz/6i8OxGyOIgYwTC')`);
+        await queryRunner.query(`INSERT INTO user VALUES (3, 'tone', '$2a$08$JA.JgQ4jSyv4rZn7FpZyfeJlStxLGD7b7aGEdz/6i8OxGyOIgYwTC')`);
+        await queryRunner.query(`INSERT INTO user VALUES (4, 'filipe', '$2a$08$JA.JgQ4jSyv4rZn7FpZyfeJlStxLGD7b7aGEdz/6i8OxGyOIgYwTC')`);
+        await queryRunner.query(`INSERT INTO user VALUES (5, 'leonor', '$2a$08$JA.JgQ4jSyv4rZn7FpZyfeJlStxLGD7b7aGEdz/6i8OxGyOIgYwTC')`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
