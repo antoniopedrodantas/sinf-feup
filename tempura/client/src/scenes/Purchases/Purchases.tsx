ad34acd86dd86cbeb58a26ac57aa064c18856c59
vitorhugo13 <vhlg99@gmail.com>
2020-12-07 12:19:17 +0000

    LineChart improvement- receiving data dynamically

--- tempura/client/src/scenes/Purchases/Purchases.tsx
+++ tempura/client/src/scenes/Purchases/Purchases.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/Purchases.css';
 import PieChart from '../../components/Charts/PieChart';
+import LineChart from '../../components/Charts/LineChart';
 
 
 const Purchases: React.FC = () => {
@@ -8,13 +9,11 @@ const Purchases: React.FC = () => {
   const lables =["vitor", "bernas", "tone", "filipe", "leonor"];
   const values = ["13", "5", "3", "5", "7"];
 
-  const lables2 =["vitor", "bernas"];
-  const values2 = ["13", "5"];
   return (
     <>
       <PieChart title="a tua prima" labels={lables} data={values}/>
       <p></p>
-      <PieChart title="Supplier Region" labels={lables2} data={values2}/>
+      <LineChart title="Supplier Region" labels={lables} data={values}/>
     </>
   );
 };
