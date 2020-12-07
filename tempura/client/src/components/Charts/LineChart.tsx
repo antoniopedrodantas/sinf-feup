ceb1757aa4c0803b38f2b1729992983d6ab7e2b6
vitorhugo13 <vhlg99@gmail.com>
2020-12-07 11:42:13 +0000

    Starting LineChart component

--- tempura/client/src/components/Charts/LineChart.tsx
+++ tempura/client/src/components/Charts/LineChart.tsx
@@ -1,2 +1,27 @@
 
 
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import './styles/LineChart.css';
+
+interface PieChartProps{
+    title: String;
+    labels: String[];
+    data: String[];
+}
+
+const LineChart: React.FC<PieChartProps> = ({title, labels, data}) => {
+
+    return(
+        <>
+           <div className="pie-chart">
+                <h3 className="chart-title">{title}</h3>
+                <div className="chart-values">
+                    
+                </div>
+            </div>
+        </>
+    );
+
+};
+export default LineChart;
