d3ebc6edcd2d442a891502414d3c30ff59ec493e
vitorhugo13 <vhlg99@gmail.com>
2020-12-08 11:47:26 +0000

    product info component started

--- tempura/client/src/scenes/DrillDowns/Client/Client.tsx
+++ tempura/client/src/scenes/DrillDowns/Client/Client.tsx
@@ -11,11 +11,16 @@ const Client: React.FC = () => {
     const titles2=["Entity", "Name", "Country", "Tax ID", "Email", "Phone"];
     const values2=["LRLDA", "L. Ribeiro, Lda.", "Portugal", "502607564", "geral@lribeiro.pt"];
 
+    const titles3 = ["Name", "Description", "Main Supplier", "Bar Code"];
+    const values3 = ["Tempura XL", "Larger Tempuras", "Kaizuya, Co. Ltd", "0 87645869 54689"];
+
     return (
         <>
             <DrillInfo title="Client Info" fields={titles} values={values}/>
             <p></p>
             <DrillInfo title="Client Info" fields={titles2} values={values2}/>
+            <p></p>
+            <DrillInfo title="Product Info" fields={titles3} values={values3}/>
         </>
     );
 };
