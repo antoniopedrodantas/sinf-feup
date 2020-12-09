0c09b21b07cadb551d8048df81a1a1b60c927354
vitorhugo13 <vhlg99@gmail.com>
2020-12-09 18:23:50 +0000

    Styling needed

--- tempura/client/src/scenes/Overview/Overview.tsx
+++ tempura/client/src/scenes/Overview/Overview.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes, faCalendar } from '@fortawesome/free-solid-svg-icons'
+import { useHistory } from 'react-router-dom';
+import jwt from 'jsonwebtoken';
+import { Button } from 'react-bootstrap';
 
 import SideBar from '../../components/SideBar/SideBar';
 import Calendar from '../../components/Calendar/Calendar';
 import SingleValueCard from 'src/components/SingleValueCard/SingleValueCard';
+import CustomTable from '../../components/CustomTable/CustomTable';
+import LineChart from '../../components/Charts/LineChart';
 
 import './styles/Overview.css';
 import '../../common.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes, faCalendar } from '@fortawesome/free-solid-svg-icons'
 
-import { useHistory } from 'react-router-dom';
-import jwt from 'jsonwebtoken';
-import { Button } from 'react-bootstrap';
 
 interface TokenPayload {
   id: string;
@@ -56,6 +58,18 @@ const Overview: React.FC = () => {
   // Frontend
   const [showDatePicker, setShowDatePicker] = useState(false);
 
+  const columns1 = ["Name", "Sold Units", "Price"];
+  const types1 = ["text", "number", "number"];
+  const values1 = [
+      ["Sashimi", "150", "17.8"],
+      ["Tempura", "121", "18.8"],
+      ["Sushi", "103", "20.0"],
+      ["Robata", "89", "9.2"]
+  ];
+
+  const labels2 = ["Jan", "Feb", "Mar", "Apr", "May", "June"];
+  const values2 = ["50", "40", "45", "30", "52", "30"];
+
   return (
     <>
       <div className="frame"> 
@@ -81,8 +95,14 @@ const Overview: React.FC = () => {
                   </Button>
                   {showDatePicker && <Calendar start={new Date()} end={new Date(2021,0,30)}/>} 
                 </div>
-                
 
+                <SingleValueCard type="money" title="Total Profit" value={352100}/>
+                <SingleValueCard type="money" title="Total Revenue" value={500309}/>
+                <SingleValueCard type="percentage" title="Liquidity" value={67.3}/>
+                <SingleValueCard type="money" title="Total Costs" value={148209}/>
+                <CustomTable title="Top Selling Products" columns={columns1} type={types1} values={values1} />
+                <LineChart title="Revenue Growth" labels={labels2} data={values2}/>
+                
               </div>
             </div>
           </div>
