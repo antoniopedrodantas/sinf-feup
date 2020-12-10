1bea286cc552cb5145d53220e9e9fbeddb843db6
Bernardo Santos <bernardosantos670@gmail.com>
2020-12-10 16:20:48 +0000

    Implemented top products route

--- tempura/server/src/routes/client.ts
+++ tempura/server/src/routes/client.ts
@@ -42,15 +42,15 @@ async function info(request: Request, response: Response, next: NextFunction) {
     }
 
     const customer = customers[clientID];
-    
+
     response
         .status(200)
         .send({
             "name": customer.CompanyName,
             "country": customer.BillingAddress.Country,
             "taxID": customer.CustomerTaxID,
-            "email": (customer.Email === "Desconhecido")? "no information" : customer.Email,
-            "phone": (customer.Telephone === "Desconhecido")? "no information" : customer.Telephone
+            "email": (customer.Email === "Desconhecido") ? "no information" : customer.Email,
+            "phone": (customer.Telephone === "Desconhecido") ? "no information" : customer.Telephone
         })
 }
 
@@ -68,7 +68,7 @@ async function total_sales(request: Request, response: Response, next: NextFunct
         const jsonObj = JSON.parse(fs.readFileSync(saft.path).toString());
 
         const salesInvoices = jsonObj.SourceDocuments.SalesInvoices;
-        
+
         if (!salesInvoices.CustomerInvoice.hasOwnProperty(clientID)) {
             return;
         }
@@ -97,8 +97,74 @@ async function accounts_receivable(request: Request, response: Response, next: N
 }
 
 async function top_products_purchased(request: Request, response: Response, next: NextFunction) {
-    // TODO: implement this endpoint
-    response.send('NOT IMPLEMENTED');
+    const clientID = request.params.id;
+    const start = request.query.start_date;
+    const end = request.query.end_date;
+
+    // TODO: add user parameter to query
+    const safts = await getSaftFiles(TaxAccountingBasis.BILLING, start, end);
+
+    if (safts.length == 0) {
+        // TODO: add descriptive error message and status code
+        return next(new HttpException(500, "Internal server error."))
+    }
+
+    let tmpProducts: Array<any> = [];
+
+    safts.forEach(saft => {
+        const jsonObj = JSON.parse(fs.readFileSync(saft.path).toString());
+        const salesInvoices = jsonObj.SourceDocuments.SalesInvoices;
+
+        if (!salesInvoices.CustomerInvoice.hasOwnProperty(clientID)) {
+            return;
+        }
+
+        const customerInvoices: Array<string> = salesInvoices.CustomerInvoice[clientID];
+        
+        customerInvoices.forEach(invoiceID => {
+            if (!salesInvoices.Invoice.hasOwnProperty(invoiceID)) {
+                return;
+            }
+
+            const lines: Array<any> = salesInvoices.Invoice[invoiceID].Line;
+            lines.forEach(line => {
+                const productID = line.ProductCode;
+
+                if (tmpProducts.hasOwnProperty(productID)) {
+                    tmpProducts[productID].units += line.Quantity;
+                } else {
+                    // TODO: product names are the same as codes,
+                    // probably need to get the names from jasmin
+                    tmpProducts[productID] = {
+                        id: productID,
+                        name: productID, 
+                        units: line.Quantity
+                    };
+                }
+            });
+        });
+    })
+
+    let finalProducts = [];
+    for (let code in tmpProducts) {
+        finalProducts.push(tmpProducts[code])
+        delete tmpProducts[code];
+    }
+
+    finalProducts.sort((obj1: any, obj2: any) => {
+        if (obj1.units > obj2.units) {
+            return 1;
+        } else if (obj1.units < obj2.units) {
+            return -1;
+        }    
+        return 0;
+    })
+
+    response
+        .status(200)
+        .send({
+            products: finalProducts
+        })
 }
 
 export default router;
