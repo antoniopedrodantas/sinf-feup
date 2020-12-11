aaef3bf54b56ccfbe497ef298e1b0eccc26e6a7e
vitorhugo13 <vhlg99@gmail.com>
2020-12-11 19:11:25 +0000

    Added structure to 404 page

--- tempura/client/src/scenes/Static/NotFound.tsx
+++ tempura/client/src/scenes/Static/NotFound.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React , { useEffect }from 'react';
+import { useHistory } from 'react-router-dom';
+import jwt from 'jsonwebtoken';
+const logo =  require("../../assets/cry.gif")
 import './styles/NotFound.css';
 
+interface TokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
 
 const NotFound: React.FC = () => {
+
+  const history = useHistory();
+
+  // checks for authentication
+  useEffect(() => {
+
+    // gets auth-token from the local storage
+    const token = localStorage.getItem("auth-token");
+
+    // token is not null
+    if(token != null){
+
+      try{
+
+        // gets data from token
+        // TODO: change secret and add to a .env file possibly
+        const data = jwt.verify(token, 'secret');
+        const { id } = data as TokenPayload;
+
+        // TODO: maybe do something with id later on
+        console.log("User ID: ", id);
+
+      } catch(err) {
+        history.push('/login');
+      }
+
+    }
+    else{
+      history.push('/login');
+    }
+
+  }, []);
+
   return (
     <>
-      <p className="not_found">This is the 404 page :o </p>
+
+      <div className="error-div">
+        <img id="logo" src={logo} alt="logo" />
+        <h3 className="error-404">404 Page not found!</h3>
+        <p className="error-404-p">
+          The page you are looking for does not exist...
+        </p>
+      </div>
+
     </>
   );
 };
