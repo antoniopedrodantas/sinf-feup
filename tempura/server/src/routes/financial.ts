0b324fe64e930b327415606103c22294f2f1ea61
Pedro Dantas <pedro.dantascv@gmail.com>
2020-12-11 00:13:55 +0000

    fixed minor merge bug

--- tempura/server/src/routes/financial.ts
+++ tempura/server/src/routes/financial.ts
@@ -535,7 +535,7 @@ async function resultsDemonstration(request: Request, response: Response, next: 
     const end = request.query.end_date;
 
     // TODO: add user param to query
-    const safts = await getSaftFiles(start, end);
+    const safts = await getSaftFiles(TaxAccountingBasis.ACCOUNTING, start, end);
 
     // balance sheet fields
 
