c0776ed5d30db3ebb2efb66ed491aba9c9dfe3c3
Pedro Dantas <pedro.dantascv@gmail.com>
2020-12-12 17:36:22 +0000

    added top selling products endpoint

--- tempura/server/src/lib/miscellaneous.ts
+++ tempura/server/src/lib/miscellaneous.ts
@@ -9,3 +9,41 @@ export function getRevenueTotal(bills: any) {
     return revenueTotal;
 
 }
+
+export function getAllProducts(bills: any) {
+
+    let products = [];
+
+    for(let key in bills){
+        products.push([key, 0]);
+    }
+
+    return products;
+
+}
+
+export function getTopSellingProducts(bills:any, products:any){
+
+    let lines = [];
+
+    for(let key in bills){
+
+        lines = bills[key]["Line"];
+
+        lines.forEach((line: any) => {
+            const productCode = line["ProductCode"];
+            const productQuantity = line["Quantity"];
+
+            products.forEach((product: any) => {
+                if(product[0] == productCode){
+                    product[1] += parseInt(productQuantity);
+                }
+            });
+
+        });
+
+    }
+
+    return products;
+
+}
--- tempura/server/src/routes/miscellaneous.ts
+++ tempura/server/src/routes/miscellaneous.ts
@@ -7,7 +7,8 @@ import { getSaftFiles } from "../lib/saft";
 import { TaxAccountingBasis } from "../entity/Saft";
 import HttpException from "../exceptions/HttpException";
 import fs from "fs";
-import { getRevenueTotal } from "../lib/miscellaneous";
+import { getRevenueTotal, getAllProducts, getTopSellingProducts } from "../lib/miscellaneous";
+import { json } from "body-parser";
 
 const router = express.Router();
 
@@ -53,9 +54,53 @@ function revenue_growth(request: Request, response: Response, next: NextFunction
     response.send('NOT IMPLEMENTED');
 }
 
-function top_selling_products(request: Request, response: Response, next: NextFunction) {
-    // TODO: implement this endpoint
-    response.send('NOT IMPLEMENTED');
+async function top_selling_products(request: Request, response: Response, next: NextFunction) {
+    
+    const start = request.query.start_date;
+    const end = request.query.end_date;
+
+    // TODO: add user parameter to query
+    const safts = await getSaftFiles(TaxAccountingBasis.BILLING, start, end);
+
+    if (safts.length == 0) {
+        // TODO: add descriptive error message and status code
+        return next(new HttpException(500, "Internal server error."))
+    }
+
+    // TODO: getting the first saft of the list is temporary
+    const bills = JSON.parse(fs.readFileSync(safts[0].path).toString())["SourceDocuments"]["SalesInvoices"]["Invoice"];
+    
+    // gets the bills and all products available
+    const productBills = JSON.parse(fs.readFileSync(safts[0].path).toString())["SourceDocuments"]["SalesInvoices"]["ProductInvoice"];
+    const products = getAllProducts(productBills); 
+
+    // gets top selling products and then orders them
+    const topSellingProducts = getTopSellingProducts(bills, products);
+    topSellingProducts.sort(function(a: number[], b: number[]){
+        if(a[1] < b[1]){
+            return 1;
+        }
+        if(a[1] > b[1]){
+            return -1;
+        }
+        return 0;
+    });
+
+    // creates JSON response
+    let jsonResponse: { name: any; quantity: any; }[] = [];
+    topSellingProducts.forEach((product: any) => {
+        jsonResponse.push({
+            "name": product[0],
+            "quantity": product[1],
+        })
+    });
+
+    // sends response
+    response
+        .status(200)
+        .send({
+            "products": jsonResponse,
+        });
 }
 
 export default router;
