fc9af420c69463619d050a436f418c8ad0eb4311
Bernardo Santos <bernardosantos670@gmail.com>
2020-12-12 15:16:24 +0000

    Implemented average sale price endpoint

--- tempura/server/src/routes/sales.ts
+++ tempura/server/src/routes/sales.ts
@@ -82,8 +82,31 @@ async function top_clients(request: Request, response: Response, next: NextFunct
 }
 
 async function average_sale_price(request: Request, response: Response, next: NextFunction) {
-    // TODO: implement this endpoint
-    response.send('NOT IMPLEMENTED');
+    const start = request.query.start_date;
+    const end = request.query.end_date;
+
+    const safts = await getSaftFiles(TaxAccountingBasis.BILLING, start, end);
+
+    if (safts.length == 0) {
+        // TODO: add descriptive error message and status code
+        return next(new HttpException(500, "Internal server error."))
+    }
+
+    // TODO: getting the first saft on the list is temporary
+    const jsonObj = JSON.parse(fs.readFileSync(safts[0].path).toString());
+
+    let sum: number = 0, counter: number = 0;
+    const invoicesObj = jsonObj.SourceDocuments.SalesInvoices.Invoice
+    for (let invoiceID in invoicesObj) {
+        sum += parseFloat(invoicesObj[invoiceID].DocumentTotals.NetTotal);
+        counter++;
+    }
+
+    response
+        .status(200)
+        .send({
+            avg_sale: sum / counter
+    });
 }
 
 export default router;
