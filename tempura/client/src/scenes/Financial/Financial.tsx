b86d3e64562ae0c7697c591b1c3ae1ca4d28ac5a
leonormfreitas <up201207603@fe.up.pt>
2020-12-12 22:56:21 +0000

    Uncommented login part

--- tempura/client/src/scenes/Financial/Financial.tsx
+++ tempura/client/src/scenes/Financial/Financial.tsx
@@ -201,37 +201,37 @@ export const Financial: React.FC = () => {
   const history = useHistory();
 
   // checks for authentication
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   // gets auth-token from the local storage
-  //   const token = localStorage.getItem("auth-token");
+    // gets auth-token from the local storage
+    const token = localStorage.getItem("auth-token");
 
-  //   // token is not null
-  //   if(token != null){
+    // token is not null
+    if(token != null){
 
-  //     try{
+      try{
 
-  //       // gets data from token
-  //       // TODO: change secret and add to a .env file possibly
-  //       const data = jwt.verify(token, 'secret');
+        // gets data from token
+        // TODO: change secret and add to a .env file possibly
+        const data = jwt.verify(token, 'secret');
 
-  //       // gets user id from user
-  //       const { id } = data as TokenPayload;
+        // gets user id from user
+        const { id } = data as TokenPayload;
 
-  //       // TODO: maybe do something with id later on
-  //       console.log("User ID: ", id);
+        // TODO: maybe do something with id later on
+        console.log("User ID: ", id);
 
-  //     } catch(err) {
-  //       history.push('/login');
-  //     }
+      } catch(err) {
+        history.push('/login');
+      }
 
-  //   }
-  //   else{
-  //     // redirects to login
-  //     history.push('/login');
-  //   }
+    }
+    else{
+      // redirects to login
+      history.push('/login');
+    }
 
-  // }, []);
+  }, []);
 
   // Frontend
 
