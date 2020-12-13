8175661c8301fe40e8c1ec22b772fb5772a02eb0
Bernardo Santos <bernardosantos670@gmail.com>
2020-12-13 14:49:43 +0000

    Implemented total_costs endpoint

--- tempura/server/src/routes/overview.ts
+++ tempura/server/src/routes/overview.ts
@@ -1,4 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
+import HttpException from "../exceptions/HttpException";
+import JasminRequester from "../lib/JasminRequester";
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
 
 import asyncMiddleware from "../middlewares/asyncMiddleware";
 import authMiddleware from "../middlewares/authMiddleware";
@@ -16,8 +20,38 @@ async function total_profit(request: Request, response: Response, next: NextFunc
 }
 
 async function total_costs(request: Request, response: Response, next: NextFunction) {
-    // TODO: implement this endpoint
-    response.send('NOT IMPLEMENTED');
+    let user = await getRepository(User).findOne({ where: { id: request.user } });
+    if (!user) {
+        return next(new HttpException(500, "User missing"));
+    }
+
+    const startDate = request.body.start_date;
+    const endDate = request.body.end_date;
+
+    let jasminRequest = new JasminRequester(user);
+    try {
+        let jasminResponse = (await jasminRequest.getAllPurchaseOrders()).data;
+
+        let value = jasminResponse.reduce(
+            (accumulator, order) => {
+                const checkStartDate = (!!startDate && new Date(order.documentDate) >= new Date(startDate)) || !startDate;
+                const checkEndDate = (!!endDate && new Date(order.documentDate) <= new Date(endDate)) || !endDate;
+
+                if (checkStartDate && checkEndDate) {
+                    accumulator += order.grossValue.amount;
+                }
+
+                return accumulator
+            }, 0);
+
+        response
+            .status(200)
+            .send({
+                total_costs: value
+            })
+    } catch (error) {
+        return next(new HttpException(500, "Server Error"));
+    }
 }
 
 async function liquidity(request: Request, response: Response, next: NextFunction) {
