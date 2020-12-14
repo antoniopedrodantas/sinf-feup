5e73a95167dd3594497b668242cba01a1d8a43c4
Bernardo Santos <bernardosantos670@gmail.com>
2020-12-14 03:23:51 +0000

    removed awaits from all pages

--- tempura/client/src/scenes/DrillDowns/Product/Product.tsx
+++ tempura/client/src/scenes/DrillDowns/Product/Product.tsx
@@ -26,7 +26,7 @@ const Product: React.FC = () => {
 
     let pageURL = window.location.href;
     let productID = pageURL.substr(pageURL.lastIndexOf('/') + 1);
-    console.log( "Product ID: " + productID);
+    console.log("Product ID: " + productID);
 
     // json request body
     const body = {
@@ -46,9 +46,9 @@ const Product: React.FC = () => {
         }
     );
     const [totalUnitsSold, setTotalUnitsSold] = useState(
-      {
-          units: 0,
-      }  
+        {
+            units: 0,
+        }
     );
     const [unitsSoldPerDay, setUnitsSoldPerDay] = useState([
         {
@@ -79,116 +79,116 @@ const Product: React.FC = () => {
 
     const history = useHistory();
 
-        // checks for authentication
-        useEffect(() => {
-            (async () => {
+    // checks for authentication
+    useEffect(() => {
+        (async () => {
 
-                // gets auth-token from the local storage
-                const token = localStorage.getItem("auth-token");
-        
-                // token is not null
-                if(token != null){
-        
-                try{
-        
+            // gets auth-token from the local storage
+            const token = localStorage.getItem("auth-token");
+
+            // token is not null
+            if (token != null) {
+
+                try {
+
                     // gets data from token
                     // TODO: change secret and add to a .env file possibly
                     const data = jwt.verify(token, 'secret');
-        
+
                     // gets user id from user
                     const { id } = data as TokenPayload;
-        
+
                     // TODO: maybe do something with id later on
                     console.log("User ID: ", id);
-        
-                } catch(err) {
+
+                } catch (err) {
                     history.push('/login');
                 }
-        
-                }
-                else{
+
+            }
+            else {
                 // redirects to login
                 history.push('/login');
-                }
+            }
 
-                // gets product's info
-                await axios.get(`http://localhost:8000/product/${productID}/info?start_date=2020-01-01 00:00:00&end_date=2021-01-01 00:00:00`, {
-                        headers: { 'authorization': token },
-                    }).then((res) => {
-                        setProductInfo(res.data);
-                    }).catch((err) => {
-                        console.log(err);
-                    });
+            // gets product's info
+            axios.get(`http://localhost:8000/product/${productID}/info?start_date=2020-01-01 00:00:00&end_date=2021-01-01 00:00:00`, {
+                headers: { 'authorization': token },
+            }).then((res) => {
+                setProductInfo(res.data);
+            }).catch((err) => {
+                console.log(err);
+            });
 
-                // gets total units sold
-                await axios.post(`http://localhost:8000/product/${productID}/total_units_sold`, formurlencoded(body), {
-                        headers: { 
-                            'authorization': token,
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    }).then((res) => {
-                        setTotalUnitsSold(res.data);
-                    }).catch((err: any) => {
-                        console.log(err);
-                    });
+            // gets total units sold
+            axios.post(`http://localhost:8000/product/${productID}/total_units_sold`, formurlencoded(body), {
+                headers: {
+                    'authorization': token,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }).then((res) => {
+                setTotalUnitsSold(res.data);
+            }).catch((err: any) => {
+                console.log(err);
+            });
 
-                // gets units sold per day
-                await axios.get(`http://localhost:8000/product/${productID}/units_sold_per_day?start_date=2020-01-01 00:00:00&end_date=2021-01-01 00:00:00`, {
-                        headers: { 'authorization': token },
-                    }).then((res) => {
-                        setUnitsSoldPerDay(res.data["Units Per Day"]);
-                    }).catch((err) => {
-                        console.log(err);
-                    });
+            // gets units sold per day
+            axios.get(`http://localhost:8000/product/${productID}/units_sold_per_day?start_date=2020-01-01 00:00:00&end_date=2021-01-01 00:00:00`, {
+                headers: { 'authorization': token },
+            }).then((res) => {
+                setUnitsSoldPerDay(res.data["Units Per Day"]);
+            }).catch((err) => {
+                console.log(err);
+            });
 
-                // gets average purchase price
-                await axios.post(`http://localhost:8000/product/${productID}/average_purchase_price`, formurlencoded(body), {
-                        headers: { 
-                            'authorization': token,
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    }).then((res) => {
-                        setPurchasePrice(res.data);
-                    }).catch((err: any) => {
-                        console.log(err);
-                    });
+            // gets average purchase price
+            axios.post(`http://localhost:8000/product/${productID}/average_purchase_price`, formurlencoded(body), {
+                headers: {
+                    'authorization': token,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }).then((res) => {
+                setPurchasePrice(res.data);
+            }).catch((err: any) => {
+                console.log(err);
+            });
 
-                // gets average sales price
-                await axios.post(`http://localhost:8000/product/${productID}/average_sale_price`, formurlencoded(body), {
-                        headers: { 
-                            'authorization': token,
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    }).then((res) => {
-                        setSalePrice(res.data);
-                    }).catch((err: any) => {
-                        console.log(err);
-                    });
+            // gets average sales price
+            axios.post(`http://localhost:8000/product/${productID}/average_sale_price`, formurlencoded(body), {
+                headers: {
+                    'authorization': token,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }).then((res) => {
+                setSalePrice(res.data);
+            }).catch((err: any) => {
+                console.log(err);
+            });
 
-                // gets average profit per unit
-                await axios.post(`http://localhost:8000/product/${productID}/average_profit_per_unit`, formurlencoded(body), {
-                        headers: { 
-                            'authorization': token,
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    }).then((res) => {
-                        setProfit(res.data);
-                    }).catch((err: any) => {
-                        console.log(err);
-                    });
+            // gets average profit per unit
+            axios.post(`http://localhost:8000/product/${productID}/average_profit_per_unit`, formurlencoded(body), {
+                headers: {
+                    'authorization': token,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }).then((res) => {
+                setProfit(res.data);
+            }).catch((err: any) => {
+                console.log(err);
+            });
 
-                // gets units in stock
-                await axios.get(`http://localhost:8000/product/${productID}/units_in_stock?start_date=2020-01-01 00:00:00&end_date=2021-01-01 00:00:00`, {
-                        headers: { 'authorization': token },
-                    }).then((res) => {
-                        setUnitsInStock(res.data);
-                    }).catch((err) => {
-                        console.log(err);
-                    });
+            // gets units in stock
+            axios.get(`http://localhost:8000/product/${productID}/units_in_stock?start_date=2020-01-01 00:00:00&end_date=2021-01-01 00:00:00`, {
+                headers: { 'authorization': token },
+            }).then((res) => {
+                setUnitsInStock(res.data);
+            }).catch((err) => {
+                console.log(err);
+            });
 
-            })();
-    
-        }, []);
+        })();
+
+    }, []);
 
     const titles = ["Name", "Description", "Main Supplier", "Bar Code"];
     const values = [productInfo.name, productInfo.description, productInfo.main_supplier.name, productInfo.bar_code];
@@ -199,9 +199,9 @@ const Product: React.FC = () => {
 
     // const labels2 = ["Jan", "Feb", "Mar", "Apr", "May", "June"];
     // const values2 = ["50", "40", "45", "30", "52", "30"];
-    const labels2:Array<any> = [];
-    const values2:Array<any> = [];
-    unitsSoldPerDay.map((day:any) => {
+    const labels2: Array<any> = [];
+    const values2: Array<any> = [];
+    unitsSoldPerDay.map((day: any) => {
         labels2.push(day.day);
         values2.push(day.quantity);
     });
@@ -209,46 +209,46 @@ const Product: React.FC = () => {
 
     return (
         <>
-            <div className="frame"> 
+            <div className="frame">
 
                 <input type="checkbox" id="menu" defaultChecked={true}></input>
 
                 <div className="row h-100">
                     <div className="left-side col-md-2">
-                        <label htmlFor="menu" className="menu-close"><FontAwesomeIcon icon={faTimes} className="toggle-icon"/></label>
-                        <SideBar coreview="drill"/>
+                        <label htmlFor="menu" className="menu-close"><FontAwesomeIcon icon={faTimes} className="toggle-icon" /></label>
+                        <SideBar coreview="drill" />
                     </div>
 
                     <div className="right-side col-md-10">
                         <div className="toggle-menu">
                             <div className="tempura"> Tempura</div>
-                            <label htmlFor="menu" className="menu-bar"><FontAwesomeIcon icon={faBars} className="toggle-icon"/></label>
+                            <label htmlFor="menu" className="menu-bar"><FontAwesomeIcon icon={faBars} className="toggle-icon" /></label>
                         </div>
                         <div className="right-body">
                             <div className="product-content">
                                 <div className="date-selection">
-                                    <Button onClick={()=> setShowDatePicker(!showDatePicker)}className="date-btn" variant="outlined"> <FontAwesomeIcon icon={faCalendar} className="calendar-icon"/> 
+                                    <Button onClick={() => setShowDatePicker(!showDatePicker)} className="date-btn" variant="outlined"> <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
                                         {showDatePicker ? "Hide" : "Date Picker"}
                                     </Button>
-                                    {showDatePicker && <Calendar start={new Date()} end={new Date(2021,0,30)}/>} 
+                                    {showDatePicker && <Calendar start={new Date()} end={new Date(2021, 0, 30)} />}
                                 </div>
-                                
-                                
+
+
                                 <div className="all-info-product">
                                     <div className="main-info-product">
-                                    <DrillInfo title="Product Info" fields={titles} values={values} supplierID={productInfo.main_supplier.id}/>
-                                    <LineChart title="Units Sold" labels={labels2} data={values2} width={600}/>
+                                        <DrillInfo title="Product Info" fields={titles} values={values} supplierID={productInfo.main_supplier.id} />
+                                        <LineChart title="Units Sold" labels={labels2} data={values2} width={600} />
                                     </div>
                                     <div className="single-card-col">
-                                        <SingleValueCard type="text" title="Total Units Sold" value={totalUnitsSold.units.toString()}/>
-                                        <SingleValueCard type="text" title="Units in Stock" value={unitsInStock.data.toString()}/>
-                                        <SingleValueCard type="money" title="Avg. Puchase Price" value={purchasePrice.average_purchase_price}/>
-                                        <SingleValueCard type="money" title="Avg. Sale Price" value={salePrice.average_sale_price}/>
-                                        <SingleValueCard type="money" title="Avg. Profit per Unit" value={profit.average_profit}/>
+                                        <SingleValueCard type="text" title="Total Units Sold" value={totalUnitsSold.units.toString()} />
+                                        <SingleValueCard type="text" title="Units in Stock" value={unitsInStock.data.toString()} />
+                                        <SingleValueCard type="money" title="Avg. Puchase Price" value={purchasePrice.average_purchase_price} />
+                                        <SingleValueCard type="money" title="Avg. Sale Price" value={salePrice.average_sale_price} />
+                                        <SingleValueCard type="money" title="Avg. Profit per Unit" value={profit.average_profit} />
                                     </div>
-                                    
+
                                 </div>
-                                
+
                             </div>
                         </div>
                     </div>
--- tempura/client/src/scenes/Financial/Financial.tsx
+++ tempura/client/src/scenes/Financial/Financial.tsx
@@ -9,7 +9,7 @@ import SideBar from '../../components/SideBar/SideBar';
 import './styles/Financial.css';
 import '../../common.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes, faCalendar} from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes, faCalendar } from '@fortawesome/free-solid-svg-icons'
 
 import { useHistory } from 'react-router-dom';
 import jwt from 'jsonwebtoken';
@@ -61,41 +61,41 @@ export const Financial: React.FC = () => {
       },
       "Liabilities": {
         "Non Current Liabilities": {
-            "Provisions": 0,
-            "Financing Obtained": 0,
-            "Responsibilities for Post-Employment Benefits": 0,
-            "Deferred Tax Liabilities": 0,
-            "Accounts Payable": 0,
-            "Sum": 0
+          "Provisions": 0,
+          "Financing Obtained": 0,
+          "Responsibilities for Post-Employment Benefits": 0,
+          "Deferred Tax Liabilities": 0,
+          "Accounts Payable": 0,
+          "Sum": 0
         },
         "Current Liabilities": {
-            "Suppliers": 0,
-            "Client Advances": 0,
-            "Government and Other Public Entities": 0,
-            "Financing Obtained": 0,
-            "Other Accounts Payable": 0,
-            "Deferrals": 0,
-            "Financial Liabilities Held for Trading": 0,
-            "Other Financial Liabilities": 0,
-            "Non-Current Liabilities Held for Sale": 0,
-            "Other Current Liabilities": 0,
-            "Sum": 0
+          "Suppliers": 0,
+          "Client Advances": 0,
+          "Government and Other Public Entities": 0,
+          "Financing Obtained": 0,
+          "Other Accounts Payable": 0,
+          "Deferrals": 0,
+          "Financial Liabilities Held for Trading": 0,
+          "Other Financial Liabilities": 0,
+          "Non-Current Liabilities Held for Sale": 0,
+          "Other Current Liabilities": 0,
+          "Sum": 0
         },
         "Total Liabilities": 0
       },
       "Equity": {
         "Equity": {
-            "Subscribed Capital": 0,
-            "Shares": 0,
-            "Other Equity Instruments": 0,
-            "Issue Premiums": 0,
-            "Legal Reserves": 0,
-            "Other Reserves": 0,
-            "Transited Results": 0,
-            "Revaluation Surpluses": 0,
-            "Adjustments/Other Changes in Equity": 0,
-            "Net Income for the Period": 0,
-            "Anticipated Dividends": 0
+          "Subscribed Capital": 0,
+          "Shares": 0,
+          "Other Equity Instruments": 0,
+          "Issue Premiums": 0,
+          "Legal Reserves": 0,
+          "Other Reserves": 0,
+          "Transited Results": 0,
+          "Revaluation Surpluses": 0,
+          "Adjustments/Other Changes in Equity": 0,
+          "Net Income for the Period": 0,
+          "Anticipated Dividends": 0
         },
         "Total Equity": 0
       }
@@ -143,9 +143,9 @@ export const Financial: React.FC = () => {
       const token = localStorage.getItem("auth-token");
 
       // token is not null
-      if(token != null){
+      if (token != null) {
 
-        try{
+        try {
 
           // gets data from token
           // TODO: change secret and add to a .env file possibly
@@ -157,34 +157,34 @@ export const Financial: React.FC = () => {
           // TODO: maybe do something with id later on
           console.log("User ID: ", id);
 
-        } catch(err) {
+        } catch (err) {
           history.push('/login');
         }
 
       }
-      else{
+      else {
         // redirects to login
         history.push('/login');
       }
 
       // gets balance sheet info
-      await axios.get(`http://localhost:8000/balance_sheet?start_date=2018-12-31 00:00:00&end_date=2020-01-01 00:00:00`, {
-              headers: { 'authorization': token },
-            }).then((res) => {
-              setBalanceSheet(res.data);
-            }).catch((err) => {
-              console.log(err);
-            });
+      axios.get(`http://localhost:8000/balance_sheet?start_date=2018-12-31 00:00:00&end_date=2020-01-01 00:00:00`, {
+        headers: { 'authorization': token },
+      }).then((res) => {
+        setBalanceSheet(res.data);
+      }).catch((err) => {
+        console.log(err);
+      });
 
       // gets results demonstration
-      await axios.get(`http://localhost:8000/results_demonstration?start_date=2018-12-31 00:00:00&end_date=2020-01-01 00:00:00`, {
-              headers: { 'authorization': token },
-            }).then((res) => {
-              setResultsDemonstration(res.data["Income and Expenses"]);
-              console.log(res.data);
-            }).catch((err) => {
-              console.log(err);
-            });
+      axios.get(`http://localhost:8000/results_demonstration?start_date=2018-12-31 00:00:00&end_date=2020-01-01 00:00:00`, {
+        headers: { 'authorization': token },
+      }).then((res) => {
+        setResultsDemonstration(res.data["Income and Expenses"]);
+        console.log(res.data);
+      }).catch((err) => {
+        console.log(err);
+      });
 
     })();
 
@@ -192,52 +192,52 @@ export const Financial: React.FC = () => {
 
   const [showDatePicker, setShowDatePicker] = useState(false);
 
-  function getCurrentAssetsFromJSON(json_data: any){
-    var currentAssets =  getArrayFromJSON(json_data["Assets"]["Current Assets"]);
+  function getCurrentAssetsFromJSON(json_data: any) {
+    var currentAssets = getArrayFromJSON(json_data["Assets"]["Current Assets"]);
     return currentAssets;
   }
 
-  function getNonCurrentAssetsFromJSON(json_data: any){
-    var nonCurrentAssets =  getArrayFromJSON(json_data["Assets"]["Non Current Assets"]);
+  function getNonCurrentAssetsFromJSON(json_data: any) {
+    var nonCurrentAssets = getArrayFromJSON(json_data["Assets"]["Non Current Assets"]);
     return nonCurrentAssets;
   }
 
-  function getTotalAssetsFromJSON(json_data: any){
-    var totalAssets =  json_data["Assets"]["Total Assets"];
+  function getTotalAssetsFromJSON(json_data: any) {
+    var totalAssets = json_data["Assets"]["Total Assets"];
     return totalAssets;
   }
 
-  function getCurrentLiabilitiesFromJSON(json_data: any){
-    var currentLiabilities =  getArrayFromJSON(json_data["Liabilities"]["Current Liabilities"]);
+  function getCurrentLiabilitiesFromJSON(json_data: any) {
+    var currentLiabilities = getArrayFromJSON(json_data["Liabilities"]["Current Liabilities"]);
     return currentLiabilities;
   }
 
-  function getNonCurrentLiabilitiesFromJSON(json_data: any){
-    var nonCurrentLiabilities =  getArrayFromJSON(json_data["Liabilities"]["Non Current Liabilities"]);
+  function getNonCurrentLiabilitiesFromJSON(json_data: any) {
+    var nonCurrentLiabilities = getArrayFromJSON(json_data["Liabilities"]["Non Current Liabilities"]);
     return nonCurrentLiabilities;
   }
 
-  function getTotalLiabilitiesFromJSON(json_data: any){
-    var totalLiabilities =  json_data["Liabilities"]["Total Liabilities"];
+  function getTotalLiabilitiesFromJSON(json_data: any) {
+    var totalLiabilities = json_data["Liabilities"]["Total Liabilities"];
     return totalLiabilities;
   }
 
-  function getEquityFromJSON(json_data: any){
-    var nonCurrentLiabilities =  getArrayFromJSON(json_data["Equity"]["Equity"]);
+  function getEquityFromJSON(json_data: any) {
+    var nonCurrentLiabilities = getArrayFromJSON(json_data["Equity"]["Equity"]);
     return nonCurrentLiabilities;
   }
 
-  function getTotalEquityFromJSON(json_data: any){
-    var totalLiabilities =  json_data["Equity"]["Total Equity"];
+  function getTotalEquityFromJSON(json_data: any) {
+    var totalLiabilities = json_data["Equity"]["Total Equity"];
     return totalLiabilities;
   }
 
 
   function getArrayFromJSON(json_data: any) {
-    var result =[];
+    var result = [];
 
-    for(var i in json_data)
-      result.push([i, json_data [i]]);
+    for (var i in json_data)
+      result.push([i, json_data[i]]);
 
     return result;
 
@@ -256,57 +256,57 @@ export const Financial: React.FC = () => {
 
   let infoILS = getArrayFromJSON(resultsDemonstration);
 
- 
 
-  const types =["text","money"];
+
+  const types = ["text", "money"];
 
   // Frontend
 
   return (
     <>
-      <div className="frame"> 
+      <div className="frame">
 
         <input type="checkbox" id="menu" defaultChecked={true}></input>
-        
+
 
         <div className="row h-100">
 
           <div className="left-side col-md-2">
-              <label htmlFor="menu" className="menu-close"><FontAwesomeIcon icon={faTimes} className="toggle-icon"/></label>
-              <SideBar coreview="financial"/>
+            <label htmlFor="menu" className="menu-close"><FontAwesomeIcon icon={faTimes} className="toggle-icon" /></label>
+            <SideBar coreview="financial" />
           </div>
 
           <div className="right-side col-md-10 right-financial">
             <div className="toggle-menu">
               <div className="tempura"> Tempura</div>
-              <label htmlFor="menu" className="menu-bar"><FontAwesomeIcon icon={faBars} className="toggle-icon"/></label>
+              <label htmlFor="menu" className="menu-bar"><FontAwesomeIcon icon={faBars} className="toggle-icon" /></label>
             </div>
-            
+
             <div className="right-body right-div">
 
-            <div className="financial-content">
+              <div className="financial-content">
 
-              <div className="date-selection date-selection-financial">
-                  <Button onClick={()=> setShowDatePicker(!showDatePicker)}className="date-btn" variant="outlined"> <FontAwesomeIcon icon={faCalendar} className="calendar-icon"/> 
+                <div className="date-selection date-selection-financial">
+                  <Button onClick={() => setShowDatePicker(!showDatePicker)} className="date-btn" variant="outlined"> <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
                     {showDatePicker ? "Hide" : "Date Picker"}
                   </Button>
-                  {showDatePicker && <Calendar start={new Date()} end={new Date(2021,0,30)}/>} 
-              </div>
+                  {showDatePicker && <Calendar start={new Date()} end={new Date(2021, 0, 30)} />}
+                </div>
 
-            <div className = "row h-90 row-financial">
-                <div className="tb-bs col-md-6">
-                  <BalanceSheet currentAssets={infoCurrentAssets} nonCurrentAssets={infoNonCurrentAssets} currentLiabilities={infoCurrentLiabilities} nonCurrentLiabilities={infoNonCurrentLiabilities} assetsTotal={infoTotalAssets} liabilitiesTotal={infoTotalLiabilities} equity={infoEquity} equityTotal={infoTotalEquity} types={types}/>
+                <div className="row h-90 row-financial">
+                  <div className="tb-bs col-md-6">
+                    <BalanceSheet currentAssets={infoCurrentAssets} nonCurrentAssets={infoNonCurrentAssets} currentLiabilities={infoCurrentLiabilities} nonCurrentLiabilities={infoNonCurrentLiabilities} assetsTotal={infoTotalAssets} liabilitiesTotal={infoTotalLiabilities} equity={infoEquity} equityTotal={infoTotalEquity} types={types} />
+                  </div>
+                  <div className="tb-ils col-md-6">
+                    <IncomeLossStatement accounts={infoILS} types={types} />
+                  </div>
                 </div>
-                <div className="tb-ils col-md-6">
-                  <IncomeLossStatement accounts={infoILS} types={types}/>
-                </div>
+              </div>
             </div>
-            </div>
-          </div>
           </div>
         </div>
       </div>
     </>
-  ); 
+  );
 };
 export default Financial;
